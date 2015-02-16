1b61a3bc55303c9cdc28d90f1aab1a847ae20530
Mem4o <mitev.e93@gmail.com>
2015-02-16 22:14:42 +0200

    structure code
    This is basic structure of our code.

---
+++ CarsGame/CarsGame/CarsGame.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+//създаване на обект
+struct Object
+{
+    //тези данни показват:
+    public int x; //къде по Х координатата
+    public int y; //Къде по У координатата
+    public char c; // какво тук е символ нашето ще е масив от символи
+    public ConsoleColor color; //какъв цвят
+}
+
+class CarsGame
+{
+    static void Main()
+    {
+        double speed;
+        double acceleration;
+        int playfieldWidth;
+        int livesCount;
+
+
+        Console.BufferHeight = Console.WindowHeight = 35;
+        Console.BufferWidth = Console.WindowWidth = 170;
+
+        while (true)
+        {
+            MoveUserCar();
+            NewCar();
+            HittingCars();
+            Console.Clear();
+            if (HittingCars())
+            {
+                //трябва да се изчистят всичките колички дето падат
+            }
+            else
+            {
+                //PrintOnPosition();
+            }
+            //foreach (Object car in objects)
+            //{
+            //    //PrintOnPosition();
+            //}
+            DrawInfo();
+        }
+    }
+
+    //Този метод го ползваме за принтиране на количките и текста
+    static void PrintOnPosition(int x, int y, char c, ConsoleColor color = ConsoleColor.Gray)
+    {
+
+    }
+
+    //след като свършат животите ползваме този метод.
+    static void PrintStringOnPosition(int x, int y, string str, ConsoleColor color = ConsoleColor.Gray)
+    {
+
+    }
+
+    // това е колата на user-a
+    static void MoveUserCar()
+    {
+
+    }
+
+    //този е за създаване на нова кола която пада
+    static void NewCar()
+    {
+
+    }
+
+    //проверява дали количките са се ударили
+    static bool HittingCars()
+    {
+
+        return true;// трябва да се създаде някаква промелнива която да връща bool дали са се ударили или не.
+    }
+
+    static void OldCar()
+    {
+
+    }
+
+    //този е за принтиране на инфото за животите, ускорението и т.н.
+    static void DrawInfo()
+    {
+
+    }
+}
